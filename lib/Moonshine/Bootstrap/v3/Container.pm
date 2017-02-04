86bdbce2652a4b20b2c402d9e26bdfe37291a3c4
LNATION <thisusedtobeanemail@gmail.com>
2017-02-04 18:44:12 +0100

    container()

---
+++ lib/Moonshine/Bootstrap/v3/Container.pm
@@ -0,0 +1,10 @@
+package Moonshine::Bootstrap::v3::Container;
+
+use Moonshine::Magic;
+
+use Moonshine::Bootstrap::Component::Container; 
+extends 'Moonshine::Bootstrap::Component::Container';
+
+1;
+
+
