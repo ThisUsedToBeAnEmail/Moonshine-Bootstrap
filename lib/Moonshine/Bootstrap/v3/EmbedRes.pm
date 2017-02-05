64036aa8378d78db5b7c9b65edbbb53058806bce
LNATION <thisusedtobeanemail@gmail.com>
2017-02-05 14:09:34 +0100

    embed_res()

--- lib/Moonshine/Bootstrap/v3/EmbedRes.pm
+++ lib/Moonshine/Bootstrap/v3/EmbedRes.pm
@@ -2,8 +2,15 @@ package Moonshine::Bootstrap::v3::EmbedRes;
 
 use Moonshine::Magic;
 
-use Moonshine::Bootstrap::Component::EmbedRes; 
-extends 'Moonshine::Bootstrap::Component::EmbedRes';
+require Moonshine::Bootstrap::Component::EmbedRes; 
+require Moonshine::Bootstrap::v3::EmbedResponsive;
+require Moonshine::Bootstrap::v3::EmbedResponsiveIframe;
+
+extends (
+    'Moonshine::Bootstrap::Component::EmbedRes', 
+    'Moonshine::Bootstrap::v3::EmbedResponsive',  
+    'Moonshine::Bootstrap::v3::EmbedResponsiveIframe',  
+);
 
 1;
 
