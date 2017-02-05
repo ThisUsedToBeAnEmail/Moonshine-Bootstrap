f83013a369d1c89555e9b5b9b65625c69266281c
LNATION <thisusedtobeanemail@gmail.com>
2017-02-05 14:02:26 +0100

    embed_res()

---
+++ lib/Moonshine/Bootstrap/v3/EmbedRes.pm
@@ -0,0 +1,10 @@
+package Moonshine::Bootstrap::v3::EmbedRes;
+
+use Moonshine::Magic;
+
+use Moonshine::Bootstrap::Component::EmbedRes; 
+extends 'Moonshine::Bootstrap::Component::EmbedRes';
+
+1;
+
+
